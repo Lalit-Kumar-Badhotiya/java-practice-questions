877c93dcb937250579ac515e5c192383d7c6a803
LalitKumar0day <lalitkumar.ac105@gmail.com>
2024-07-22 21:16:12 +0530

    added a question

---
+++ Area_of_isosceles_tri.java
@@ -0,0 +1,15 @@
+//4. Area Of Isosceles Triangle 
+import java.util.Scanner;
+public class Area_of_isosceles_tri {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Base:");
+        int B = in.nextInt();
+        System.out.print("Enter Height:");
+        int H = in.nextInt();
+        System.out.println((B*H)/2);
+        in.close();
+        
+    }
+    
+}
