3c2d72c55c853542271edd3f3f67537cc519a6a5
LalitKumar0day <lalitkumar.ac105@gmail.com>
2024-07-22 21:13:05 +0530

    added a question

---
+++ Area_Of_Equilateral_Triangle.java
@@ -0,0 +1,12 @@
+//7. Area Of Equilateral Triangle
+import java.util.Scanner;
+public class Area_Of_Equilateral_Triangle{
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       double root = 0.4330127019;
+        System.out.print("Enter side:");
+        double side = in.nextDouble();
+        System.out.println(root*(side*side));
+        in.close();
+    }
+}
