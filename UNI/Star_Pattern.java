bac2fd021a28dd0be853f01d61505bd3f9afa580
LalitKumar0day <lalitkumar.ac105@gmail.com>
2024-09-08 14:36:49 +0530

    added a Star_Pattern question

---
+++ UNI/Star_Pattern.java
@@ -0,0 +1,21 @@
+//Pattern of stars using loops
+/*     *
+ *     **
+ *     ***
+ *     ****
+ *     *****
+ */
+
+public class Star_Pattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+}
